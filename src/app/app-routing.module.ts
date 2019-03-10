e3540cfd86775a6be3bb47ccff7b87a598cacd02
Unknown <ferreiralox@gmail.com>
2019-03-09 21:29:15 -0300

    add hero, heroes component, routing basic elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'heroe',
+    component: HeroeComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'heroes'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
