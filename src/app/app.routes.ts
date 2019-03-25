8e42133649eb3215c85577595fda019694e2fecd
rabindranathfv <rabindranathucv@gmail.com>
2019-03-25 18:48:28 -0300

    delete some comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroes/heroe.component';
 
 export const APP_ROUTES: Routes = [
-    { 
+    {
         path: 'heroes',
         component: HeroesComponent
     },
-    { 
+    {
         path: 'heroe/:id',
         component: HeroeComponent
     },
-    { 
+    {
         path: '**',
         pathMatch: 'full' ,
         redirectTo: 'heroes'
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
-// https://angular.io/guide/http#adding-headers IMPORTANTE!!!
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Heroe } from './../interfaces/heroe.interface';
 import {map} from 'rxjs/operators';
 
 @Injectable()
 export class HeroesService {
-  // https://heroes-app-83fc9.firebaseio.com/heroes.json
-  // https://heroes-app-83fc9.firebaseio.com/heroes/1.json
-  // https://heroes-app-83fc9.firebaseio.com/heroes/1/name.json
+
   heroesUrl = 'https://heroes-1d546.firebaseio.com/heroes.json';
   heroeUrl = 'https://heroes-1d546.firebaseio.com/heroes/';
   constructor( private http: HttpClient) { }
